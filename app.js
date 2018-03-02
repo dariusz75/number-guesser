cb4b6d9c09d483cb11d0f31e462d6209e649d883
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2018-03-02 15:35:04 +0000

    validation done

---
+++ app.js
@@ -0,0 +1,36 @@
+//Games values
+let min = 1;
+let max = 10;
+let guessesLeft = 3;
+
+//UI elements
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listener for quess
+guessBtn.addEventListener('click', function(){
+	let guess = parseInt(guessInput.value);
+
+	if (!guess) {
+		setMessage('Please entrer your number', 'red')
+	}else if (guess < min || guess > max) {
+		setMessage('Please entrer the number between ' + min +' and ' + max, 'green')
+	} else if (guess && guess >= min && guess <= max) {
+		console.log(guess);
+	}
+	
+});
+
+//Validation messages
+function setMessage(messageText, messageColor) {
+	message.textContent = messageText;
+	message.style.color = messageColor;
+}
+
