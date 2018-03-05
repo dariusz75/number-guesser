1b2c66eaf04cb8b499971ae4b0eead924427c1c6
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2018-03-05 12:46:19 +0000

    finished

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let min = 1;
 let max = 10;
 let guessesLeft = 3;
-let randomNum = 5;
+let randomNum = Math.floor((Math.random() * 10) + 1);
 
 //UI elements
 const game = document.querySelector('#game');
@@ -11,6 +11,7 @@ const maxNum = document.querySelector('.max-num');
 const guessBtn = document.querySelector('#guess-btn');
 const guessInput = document.querySelector('#guess-input');
 const message = document.querySelector('.message');
+const newGame = document.querySelector('.new-game-button');
 
 minNum.textContent = min;
 maxNum.textContent = max;
@@ -22,24 +23,33 @@ guessBtn.addEventListener('click', function(){
 	if (!guess) {
 		setMessage('Please entrer your number', 'red')
 	} else if (guess < min || guess > max) {
-		setMessage('Please entrer the number between ' + min +' and ' + max, 'green')
+		setMessage('Please entrer the number between ' + min +' and ' + max, 'red')
 	} else if (guess && guess >= min && guess <= max) {
+		guess = guess;
 
 		if (guess == randomNum) {
-			setMessage('You guessed the number! You won the game.');
-		} else {
+			setMessage('YOY WON! Please start a new game.', 'green');
+			disableButton();
+		} else if (guessesLeft > 0) {
 			guessed();
-			setMessage('Your guess is ' + guess + '. You have ' + guessesLeft + ' guesses left.');
-		}
+			setMessage('Your guess is ' + guess + '. You have ' + guessesLeft + ' guesses left.', 'black');
 
-		
-		console.log(guess);
-		console.log(guessesLeft);
-		guess = guess;
+			if (guessesLeft === 0) {
+			disableButton();
+			setMessage('You have no more guesses left! Please start a new game', 'red');
+		}
+		} 
 		
 	}
 	
 });
+
+
+//Listener for resetting the game
+newGame.addEventListener('click', function() {
+	location.reload();
+});
+
 
 //Validation messages
 function setMessage(messageText, messageColor) {
@@ -48,7 +58,12 @@ function setMessage(messageText, messageColor) {
 }
 
 function guessed() {
-
 	guessesLeft = guessesLeft - 1;
-
 }
+
+function disableButton() {
+	guessBtn.classList.add("disabled");
+	guessBtn.disabled = true;
+}
+
+console.log('The winning number is ' + randomNum);
